2e3b090e0e604465b84c5e47e8d6ff1a3e24fd50
Lockwood49 <109251552+Lockwood49@users.noreply.github.com>
2023-10-21 18:41:40 -0500

    Update streamlit_app.py
    testing whitespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -6,5 +8,9 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled, Free Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+
+
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
